0aceb9389ba88e32674a5f6aca3c44ed93966bd1
Kyle Beyer <beyerk@frib.msu.edu>
2025-06-11 14:02:31 -0400

    bias to norm

--- src/rxmc/elastic_diffxs_observation.py
+++ src/rxmc/elastic_diffxs_observation.py
@@ -18,7 +18,7 @@ class ElasticDifferentialXSObservation(Observation):
     for all subentries - this would imply that the `LikelihoodModel`
     should account for the correlation (e.g. by using a
     `LikelihoodWithSystematicError` in which the attribute
-    `y_sys_err_bias` corresponds to the COMMON `ERR-SYS` value in the
+    `y_sys_err_normalization` corresponds to the COMMON `ERR-SYS` value in the
     EXFOR entry).
 
     The order of `reactions` must match the order of `measurements`.
@@ -35,10 +35,10 @@ class ElasticDifferentialXSObservation(Observation):
         measurements: list[Distribution],
         reactions: list[jitr.reactions.Reaction],
         quantity: str,
-        y_sys_err_bias: float = 0,
-        y_sys_err_offset: float = 0,
+        y_sys_err_normalization: float = 0,
+        y_sys_err_offset: np.ndarray = 0,
         lmax: int = DEFAULT_LMAX,
-        angles_rad_vis: np.ndarray = np.linspace(0, np.pi, 100),
+        angles_vis: np.ndarray = np.linspace(0, np.pi, 100),
     ):
         """
         Initialize a ReactionObservation.
@@ -52,48 +52,88 @@ class ElasticDifferentialXSObservation(Observation):
         quantity: str
             The type of quantity to be calculated (e.g., "dXS/dA",
             "dXS/dRuth", "Ay").
-        y_sys_err_bias : float
-            Systematic error bias for the y values, default is 0.
+        y_sys_err_normalization : float
+            Systematic error normalization for the y values, default is 0.
         y_sys_err_offset : float, optional
-            Systematic error offset for the y values, default is 0.0.
+            Global systematic error offset for the y values, default is 0
         lmax: int
             Maximum angular momentum, defaults to 20.
-        angles_rad_vis: np.ndarray
-            Array of angles in radians for visualization.
+        angles_vis: np.ndarray
+            Array of angles in degrees for visualization.
         """
-        super().__init__(
-            x=np.array([m.x for m in measurements]),
-            y=np.array([m.y for m in measurements]),
-            y_stat_err=np.array([m.stat_err for m in measurements]),
-            y_sys_err_bias=y_sys_err_bias,
-            y_sys_err_offset=y_sys_err_offset,
-        )
         self.measurements = measurements
+        size = sum(len(m.x) for m in measurements)
         self.n_measurements = len(measurements)
         self.reactions = reactions
         self.quantity = quantity
         self.lmax = lmax
-        self.angles_rad_vis = angles_rad_vis
+        self.angles_vis = angles_vis
+        angles_rad_vis = angles_vis * np.pi / 180
 
         check_angle_grid(angles_rad_vis, "angles_rad_vis")
 
         if len(reactions) != self.n_measurements:
             raise ValueError("Number of reactions must match number of measurements.")
 
+        measurement_quantity = measurements[0].quantity
+        if any(m.quantity != measurement_quantity for m in measurements):
+            raise ValueError(
+                "All measurements must have the same quantity, "
+                f"but got {[m.quantity for m in measurements]}"
+            )
+
         self.visualization_workspaces = []
         self.constraint_workspaces = []
-        for i, m in enumerate(measurements):
-            angles_rad_constraint = m.x
-            check_angle_grid(angles_rad_constraint, f"measurements[{i}].x")
+        x = []
+        y = []
+        y_stat_err = []
+        meas_err_normalization = []
+        meas_err_offset = []
+        for rxn, m in zip(self.reactions, self.measurements):
+            angles_rad_constraint = m.x * np.pi / 180
+            check_angle_grid(
+                angles_rad_constraint, f"x values for subentry: {m.subentry}"
+            )
             constraint_ws, vis_ws, kinematics = set_up_solver(
-                reaction=self.reactions[i],
+                reaction=rxn,
                 Elab=m.Einc,
                 angle_rad_constraint=angles_rad_constraint,
-                angle_rad_vis=self.angles_rad_vis,
+                angle_rad_vis=angles_rad_vis,
                 lmax=self.lmax,
             )
             self.constraint_workspaces.append(constraint_ws)
             self.visualization_workspaces.append(vis_ws)
+
+            # convert measurement to correct quantity and normalize to `b/sr`
+            if self.quantity == "dXS/dRuth" and m.quantity == "dXS/dA":
+                if m.y_units == "b/sr":
+                    norm = self.constraint_workspaces[-1].rutherford / 1000
+                else:
+                    raise ValueError(
+                        f"Measurement units mismatch: {m.y_units} != b/sr for subentry {m.subentry}"
+                    )
+            elif self.quantity == "dXS/dA" and m.quantity == "dXS/dRuth":
+                norm = 1000.0 / self.constraint_workspaces[-1].rutherford
+            else:
+                norm = 1.0
+                if self.quantity != m.quantity:
+                    raise ValueError(
+                        f"Quantity mismatch: {self.quantity} != {m.quantity}"
+                    )
+
+            x.append(angles_rad_constraint)
+            y.append(m.y / norm)
+            y_stat_err.append(m.y_stat_err / norm)
+            meas_err_offset.append(m.y_sys_err_offset / norm)
+            #  fractional error in normalization not normalized
+            meas_err_normalization.append(m.y_sys_err_normalization)
+
+        super().__init__(
+            x=np.concatenate(x),
+            y=np.concatenate(y),
+            y_stat_err=np.concatenate(y_stat_err),
+            y_sys_err_offset=y_sys_err_offset,
+        )
 
 
 def set_up_solver(
